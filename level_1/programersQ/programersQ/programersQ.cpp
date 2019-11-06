c564725cacd839163adbfb5696f74cf25bbbbb33
SWH <slgo123@naver.com>
2019-11-07 01:12:26 +0900

    191107

---
+++ level_1/programersQ/programersQ/programersQ.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include <typeinfo>
+#include <stdlib.h>
+
+using namespace std;
+
+bool solution(string s);
+
+int main(void)
+{
+	string s = "1234232";
+	
+	cout << solution(s);
+
+	return 0;
+}
+
+
+bool solution(string s) {
+	bool answer = true;
+	if (s.size() == 4 || s.size() == 6)
+	{
+		for (int i = 0; i < s.size(); i++) {
+			if (!(48 <= s.at(i) && s.at(i) <= 57))
+			{
+				answer = false;
+				i += s.size();
+			}
+		}
+	}
+	else answer = false;
+
+	return answer;
+}
